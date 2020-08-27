ce06fae37931a18cadc5b8375e41b09bab844fad
gauravxdhingra <grvdhingra1999@gmail.com>
2020-08-27 06:53:00 +0530

    Verse View Page

--- lib/pages/verse_view_Page.dart
+++ lib/pages/verse_view_Page.dart
@@ -1,7 +1,10 @@
 import 'package:bhagwat_geeta/provider/scraper.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+// import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'dart:math';
 
 class VerseViewPage extends StatefulWidget {
   VerseViewPage({Key key}) : super(key: key);
@@ -13,6 +16,8 @@ class VerseViewPage extends StatefulWidget {
 class _VerseViewPageState extends State<VerseViewPage> {
   bool _isLoading = true;
   bool init = false;
+  String imageUrl =
+      "https://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-140.jpg";
 
   Map<String, String> verse = {};
   @override
@@ -27,6 +32,12 @@ class _VerseViewPageState extends State<VerseViewPage> {
       final document = await provider.getWebpage(url);
       verse = provider.getFullVerse(document);
 
+      final _random = new Random();
+      int next(int min, int max) => min + _random.nextInt(max - min);
+      int number = next.call(100, 140);
+      imageUrl =
+          "https://www.bhagavad-gita.us/wp-content/uploads/2012/09/gita-$number.jpg";
+      print(imageUrl);
       // var yt = YoutubeExplode();
       // var video =
       //     await yt.videos.get('https://www.youtube.com/watch?v=bo_efYhYU2A');
@@ -51,37 +62,64 @@ class _VerseViewPageState extends State<VerseViewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _isLoading
-          ? AppBar()
-          : AppBar(
-              title: Text(verse["title"],
-                  style: TextStyle(fontFamily: 'Samarkan')),
-            ),
-      body: _isLoading
-          ? CircularProgressIndicator()
-          : SingleChildScrollView(
-              child: Column(
-                children: [
-                  Text(verse["verseSanskrit"],
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Samarkan'),
-                      textAlign: TextAlign.center),
-                  // TODO IF ENGLISH
-                  Text(verse["transliteration"],
-                      style: TextStyle(fontSize: 20),
-                      textAlign: TextAlign.center),
-                  Text(verse["wordMeanings"],
-                      style: TextStyle(fontSize: 20),
-                      textAlign: TextAlign.center),
-                  Text(verse["translation"],
-                      style: TextStyle(fontSize: 20),
-                      textAlign: TextAlign.center),
+    return SafeArea(
+      child: Scaffold(
+        body: _isLoading
+            ? CircularProgressIndicator()
+            : CustomScrollView(
+                physics: BouncingScrollPhysics(),
+                slivers: [
+                  SliverAppBar(
+                    expandedHeight: MediaQuery.of(context).size.height / 3,
+                    pinned: true,
+                    centerTitle: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      titlePadding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      centerTitle: false,
+                      collapseMode: CollapseMode.parallax,
+                      title: Text(verse["title"],
+                          style: TextStyle(fontFamily: 'Samarkan'),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center),
+                      background: Container(
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          child: Container(
+                              child: CachedNetworkImage(imageUrl: imageUrl))),
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate([
+                      Icon(FlutterIcons.quote_right_faw),
+                      Text(verse["verseSanskrit"],
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Samarkan'),
+                          textAlign: TextAlign.center),
+                      // TODO IF ENGLISH
+
+                      Text('Transliteration'),
+                      Text(verse["transliteration"],
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center),
+
+                      Text("Word Meanings"),
+                      Text(verse["wordMeanings"],
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center),
+
+                      Text("Translation"),
+                      Text(verse["translation"],
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center),
+                    ]),
+                  )
                 ],
               ),
-            ),
+      ),
     );
   }
 }
