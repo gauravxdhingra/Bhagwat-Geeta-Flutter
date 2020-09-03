f7e84e57e23e2f5924ae1e586132aa294d75a20c
gauravxdhingra <grvdhingra1999@gmail.com>
2020-09-03 15:35:39 +0530

    New Audio Plugin

--- lib/pages/play_audio.dart
+++ lib/pages/play_audio.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_exoplayer/audio_notification.dart';
-import 'package:flutter_exoplayer/audioplayer.dart';
+// import 'package:flutter_exoplayer/audio_notification.dart';
+// import 'package:flutter_exoplayer/audioplayer.dart';
 import 'package:flutter_xlider/flutter_xlider.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yte;
 
 class PlayAudio extends StatefulWidget {
@@ -22,8 +23,10 @@ class _PlayAudioState extends State<PlayAudio> {
   AudioPlayer audioPlayer;
   var _lowerValue;
   var _upperValue;
-  Result audio;
+  // Result audio;
+  AudioPlayer player;
   var x;
+  Duration duration;
 
   @override
   void didChangeDependencies() async {
@@ -50,45 +53,24 @@ class _PlayAudioState extends State<PlayAudio> {
       String streamUrl = manifest.audioOnly.last.url.toString();
       yt.close();
 
+      player = AudioPlayer();
+
+      try {
+        duration = await player.setUrl(streamUrl);
+      } catch (e) {
+        print(e);
+      }
       // String streamUrl =
       //     "https://r5---sn-qxaeen7l.googlevideo.com/videoplayback?expire=1599126875&ei=-2hQX7H0Dsimz7sPmdibuAI&ip=110.172.189.51&id=o-APDZorRMvv0f2naSBJhfy3EURyko8bCDfYwctxdmfMVR&itag=251&source=youtube&requiressl=yes&mh=Wa&mm=31,26&mn=sn-qxaeen7l,sn-cvh76nes&ms=au,onr&mv=m&mvi=5&pl=24&initcwndbps=507500&vprv=1&mime=audio/webm&gir=yes&clen=105539586&dur=6459.781&lmt=1577276482176105&mt=1599105163&fvip=5&keepalive=yes&c=WEB&txp=1301222&sparams=expire,ei,ip,id,itag,source,requiressl,vprv,mime,gir,clen,dur,lmt&sig=AOq0QJ8wRQIgPh1da00AuBMY2DA4XeSkC3SDvYo56Q1b7Ni4h22i6MwCIQDsZtaGwlcmJA8o3bRkUFS25tqXqz7vv1viba-uNTUaeg==&lsparams=mh,mm,mn,ms,mv,mvi,pl,initcwndbps&lsig=AG3C_xAwRAIgEC2Dj4yP-wOymJpi67VNycZMMbsONrm36OfzuWQqGg8CIDN9UX-LouDJ08z98OeMOwDzMp4PgAmxk38lKV17dl-9";
 
-      audioPlayer = AudioPlayer();
+      // duration = await audioPlayer.getDuration();
+      // print("***************************************");
+      // print("***************************************");
+      print(duration);
+      // print("***************************************");
+      // print("***************************************");
 
-      AudioPlayer.logEnabled = false;
-
-      // AudioObject audioObject = AudioObject(
-      //   smallIconFileName: "your icon file name",
-      //   title: "title",
-      //   subTitle: "artist",
-      //   largeIconUrl: "local or network image url",
-      //   isLocal: false,
-      // notificationDefaultActions: NotificationDefaultActions.ALL,
-      // notificationCustomActions: NotificationCustomActions.TWO,
-      // );
-
-      audio = await audioPlayer.play(
-        streamUrl,
-        playerMode: PlayerMode.BACKGROUND,
-        respectAudioFocus: true,
-        audioNotification: AudioNotification(
-          isLocal: true,
-          title: "Bhagwat Geeta",
-          subTitle: "Chapter $chapterNo, $language",
-          smallIconFileName: "assets/images/cover1.jpg",
-          notificationDefaultActions: NotificationDefaultActions.ALL,
-          notificationCustomActions: NotificationCustomActions.TWO,
-        ),
-      );
-      print(audio.toString());
-      if (audio == Result.ERROR) {
-        print("something went wrong in playAll method :(");
-      }
-      print("***************************************");
-      print(AudioPlayer.ResultMap);
-      print("***************************************");
-      print("***************************************");
-
+      player.play();
       setState(() {
         loading = false;
         init = true;
@@ -165,63 +147,59 @@ class _PlayAudioState extends State<PlayAudio> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Text("Chapter $chapterNo - $language"),
-            IconButton(
-              icon: Icon(Icons.play_arrow),
-              onPressed: () async {
-                final Result result = await audioPlayer.resume();
-                if (result == Result.FAIL) {
-                  print(
-                      "you tried to call audio conrolling methods on released audio player :(");
-                } else if (result == Result.ERROR) {
-                  print("something went wrong in resume :(");
-                }
-              },
+      body: loading
+          ? CircularProgressIndicator()
+          : Container(
+              child: Column(
+                children: [
+                  Container(),
+                  Text("Chapter $chapterNo - $language"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        onPressed: () async {
+                          player.play();
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.pause),
+                        onPressed: () async {
+                          player.pause();
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.speaker),
+                        onPressed: () async {},
+                      ),
+                    ],
+                  ),
+                  FlutterSlider(
+                    values: [_lowerValue],
+                    max: duration.inSeconds.toDouble(),
+                    min: 0,
+                    onDragCompleted:
+                        (handlerIndex, lowerValue, upperValue) async {
+                      _lowerValue = lowerValue;
+                      _upperValue = upperValue;
+                      print(lowerValue);
+                      print(x);
+                      // print(upperValue);
+                      await player.seek(Duration(seconds: lowerValue.round()));
+                      setState(() {});
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('${player.position.inSeconds}'),
+                      Text(player.duration.inSeconds.toString()),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            IconButton(
-              icon: Icon(Icons.pause),
-              onPressed: () async {
-                final Result result = await audioPlayer.pause();
-                if (result == Result.FAIL) {
-                  print(
-                      "you tried to call audio conrolling methods on released audio player :(");
-                } else if (result == Result.ERROR) {
-                  print("something went wrong in pause :(");
-                }
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.speaker),
-              onPressed: () async {
-                Duration _duration = Duration(minutes: 50);
-                final Result result = await audioPlayer.seekPosition(_duration);
-                if (result == Result.FAIL) {
-                  print(
-                      "you tried to call audio conrolling methods on released audio player :(");
-                } else if (result == Result.ERROR) {
-                  print("something went wrong in seek :(");
-                }
-              },
-            ),
-            FlutterSlider(
-              values: [_lowerValue],
-              max: 500,
-              min: 0,
-              onDragging: (handlerIndex, lowerValue, upperValue) {
-                _lowerValue = lowerValue;
-                _upperValue = upperValue;
-                print(lowerValue);
-                print(x);
-                // print(upperValue);
-                setState(() {});
-              },
-            )
-          ],
-        ),
-      ),
     ));
   }
 }
